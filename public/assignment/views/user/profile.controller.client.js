bb10b6db7cbbf700435704bcd8386b3828965c77
Harshil Katira <hkatira@healthedge.com>
2016-05-27 21:51:56 -0400

    fixed profile page update issue

--- public/assignment/views/user/profile.controller.client.js
+++ public/assignment/views/user/profile.controller.client.js
@@ -10,11 +10,13 @@
         var id = $routeParams.uid;
 
         function init() {
-            vm.user = UserService.findUserById(id);
+            vm.user = angular.copy(UserService.findUserById(id));
+            console.log(vm.user);
         }
         init();
 
         function updateUser(newUser) {
+            // console.log("here");
             var result = UserService.updateUser(id, newUser);
             if(result){
                 vm.message = "Your profile was saved."
