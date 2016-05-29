1aa7a9981c2fd796b662fe359a565c287347a1d8
Harshil Katira <hkatira@healthedge.com>
2016-05-28 23:36:48 -0400

    created NewWidgetController and EditWidgetController, widget-heading new and edit

--- public/assignment/services/widget.service.client.js
+++ public/assignment/services/widget.service.client.js
@@ -4,12 +4,12 @@
         .factory("WidgetService", WidgetService);
 
     var widgets = [
-        { "_id": "123", "widgetType": "HEADER", "pageId": "321", "size": 2, "text": "GIZMODO"},
-        { "_id": "234", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+        { "_id": "123", "widgetType": "HEADER", "pageId": "321", "size": "2", "text": "GIZMODO"},
+        { "_id": "234", "widgetType": "HEADER", "pageId": "321", "size": "4", "text": "Lorem ipsum"},
         { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
             "url": "http://lorempixel.com/400/200/"},
         { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
-        { "_id": "567", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+        { "_id": "567", "widgetType": "HEADER", "pageId": "321", "size": "4", "text": "Lorem ipsum"},
         { "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
             "url": "https://youtu.be/AM2Ivdi9c4E" },
         { "_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
@@ -29,14 +29,9 @@
             var newWidget = {
                 _id: (new Date()).getTime()+"",
                 widgetType: widget.widgetType,
-                pageID: widget.pageId,
-                width: widget.width,
-                size: widget.size,
-                text: widget.text,
-                url: widget.url
+                pageId: pageId
             };
             widgets.push(newWidget);
-            console.log(widgets);
             return newWidget;
         }
         
@@ -62,10 +57,18 @@
         function updateWidget(widgetId, widget) {
             for(var i in widgets) {
                 if(widgets[i]._id === widgetId) {
-                    widgets[i].width = widget.width;
-                    widgets[i].size = widget.size;
+                    widgets[i].name = widget.name;
                     widgets[i].text = widget.text;
-                    widgets[i].url = widget.url;
+                    switch (widget.widgetType){
+                        case "HEADER":
+                            widgets[i].size = widget.size;
+                            break;
+                        case "IMAGE":
+                        case "YOUTUBE":
+                            widgets[i].width = widget.width;
+                            widgets[i].url = widget.url;
+                            break;
+                    }
                     return true;
                 }
             }
