ed552acca1747d4998531b1e218de4c78cfa301c
Harshil Katira <hkatira@healthedge.com>
2016-06-06 10:55:21 -0400

    added user web service and login, profile controllers same as assignments

--- public/project/config.js
+++ public/project/config.js
@@ -7,19 +7,19 @@
         $routeProvider
             .when("/login", {
                 templateUrl: "views/user/login.view.client.html",
-                //controller: "LoginController",
-                //controllerAs: "model"
-            });
+                controller: "LoginController",
+                controllerAs: "model"
+            })
             /*.when("/register", {
                 templateUrl: "views/user/register.view.client.html",
                 controller: "RegisterController",
                 controllerAs: "model"
-            })
+            })*/
             .when("/user/:userId", {
                 templateUrl: "views/user/profile.view.client.html",
                 controller: "ProfileController",
                 controllerAs: "model"
-            });*/
+            });
             /*.otherwise({
                 redirectTo: "/login"
             });*/
---
+++ public/project/views/user/profile.controller.client.js
@@ -0,0 +1,33 @@
+(function () {
+    angular
+        .module("GamersBay")
+        .controller("ProfileController", ProfileController);
+
+    function ProfileController($routeParams, UserService) {
+        var vm = this;
+        vm.updateUser = updateUser;
+
+        var userId = $routeParams.userId;
+
+        function init() {
+            UserService
+                .findUserById(userId)
+                .then(function (response) {
+                    vm.user = response.data;
+                });
+        }
+        init();
+
+        function updateUser(newUser) {
+            UserService
+                .updateUser(userId, newUser)
+                .then(
+                    function (response) {
+                        vm.success = "Your profile was saved.";
+                    },
+                    function (error) {
+                        vm.error = "Error saving profile."
+                    });
+        }
+    }
+})();
