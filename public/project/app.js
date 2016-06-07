232c43b1661cb5c2a3cd21ba076d98bfeae08d7d
Harshil Katira <hkatira@healthedge.com>
2016-06-07 13:46:12 -0400

    Project: parsed html for game-detail description field

--- public/project/app.js
+++ public/project/app.js
@@ -1,3 +1,3 @@
 (function(){
-    angular.module("GamersBay", ["ngRoute"]);
+    angular.module("GamersBay", ["ngRoute", "ngSanitize"]);
 })();
--- public/project/views/game/game-detail.controller.client.js
+++ public/project/views/game/game-detail.controller.client.js
@@ -2,18 +2,22 @@
     angular
         .module("GamersBay")
         .controller("GameDetailController", GameDetailController);
-    
+
     function GameDetailController($routeParams, GameService) {
         var vm = this;
 
         vm.gameId = $routeParams.gameId;
-        
+
         function init() {
             GameService
                 .getGameById(vm.gameId)
                 .then(
                     function (response) {
                         vm.game = response.data.results;
+                        if(vm.game.description) {
+                            vm.game.description = vm.game.description
+                                .split('<img').join('<img class="img-responsive col-xs-12"');
+                        }
                     },
                     function (error) {
                         vm.error = error.status;
