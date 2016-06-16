582f70b2b4fe05525151f18cfee9860079b020ad
Harshil Katira <hkatira@healthedge.com>
2016-06-15 23:44:14 -0400

    search bar and results

--- public/project/services/game.service.client.js
+++ public/project/services/game.service.client.js
@@ -6,15 +6,21 @@
     var key = "9ffe43eed92d72de9aa7dbe4fbe8c71bde9a7bb2";
     var urlBase = "http://www.giantbomb.com/api/RESOURCE/?api_key=API_KEY&format=jsonp&json_callback=JSON_CALLBACK";
 
-    var fieldList = "&field_list=aliases,deck,description,developers,expected_release_month," +
+    var detailFieldList = "&field_list=aliases,deck,description,developers,expected_release_month," +
         "expected_release_year,franchises,genres,id,image,images,name,original_release_date," +
         "platforms,publishers,similar_games,themes,videos";
+
+    var searchFieldList = "&field_list=aliases,deck,developers,expected_release_month," +
+        "expected_release_year,franchises,genres,id,image,name,original_release_date," +
+        "platforms,publishers";
 
     var limit = "&limit=15";
 
     var sort = "&sort=number_of_user_reviews:desc";
 
     var search = "&query=QUERY&resources=game";
+
+    var page = "&page=PAGENO";
 
     function GameService($http) {
         var api = {
@@ -29,26 +35,31 @@
                 .replace("API_KEY", key)
                 .replace("RESOURCE", "games");
 
-            url = url + limit + sort;
+            url = url + limit + sort + searchFieldList + page;
+
             return $http.jsonp(url);
         }
 
         function getGameById(gameId) {
+
             var url = urlBase
                 .replace("API_KEY", key)
                 .replace("RESOURCE", "game/"+gameId);
 
-            url +=fieldList;
+            url += detailFieldList;
 
             return $http.jsonp(url);
         }
 
         function searchGames(searchTerm) {
-            var url = urlBase
+
+            var url = urlBase + search + limit + searchFieldList;
+
+            url = url
                 .replace("API_KEY", key)
-                .replace("RESOURCE", "search");
-            url = url + search + limit + sort + fieldList;
-            url = url.replace("QUERY", searchTerm);
+                .replace("RESOURCE", "search")
+                .replace("QUERY", searchTerm);
+
 
             return $http.jsonp(url);
         }
--- public/project/views/home/home.controller.client.js
+++ public/project/views/home/home.controller.client.js
@@ -5,6 +5,7 @@
 
     function HomeController(GameService) {
         var vm = this;
+        vm.searchGames = searchGames;
 
         function init() {
             GameService
@@ -19,5 +20,18 @@
                 );
         }
         init();
+
+        function searchGames(searchText) {
+            GameService
+                .searchGames(searchText)
+                .then(
+                    function (response) {
+                        vm.games = response.data.results;
+                    },
+                    function (error) {
+                        vm.games = error.status;
+                    }
+                );
+        }
     }
 })();
